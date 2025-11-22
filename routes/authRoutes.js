0387370d5dcf5ff6920d62234c8357127b77aa8e
Zakia El Baaj <zakiaelbaaj@gmail.com>
2025-11-22 11:55:10 +0100

    Fix: utiliser DB_PASS au lieu de DB_PASSWORD

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   port: parseInt(process.env.DB_PORT, 10),
   ssl: { rejectUnauthorized: false }
 });
