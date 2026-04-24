2ffd01bde66e1aa370964204e8e2d547098e12fd
Zakia El Baaj <zakiaelbaaj@gmail.com>
2026-04-25 00:17:37 +0100

    fix: use DATABASE_URL for sequelize config

--- config/index.js
+++ config/index.js
@@ -5,30 +5,22 @@
 import logger from "#config/logger.js";
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    pool: {
-      max: 20,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
-);
-
+  },
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
 // 🔥 Ajout essentiel : export db
 export const db = sequelize;
 
