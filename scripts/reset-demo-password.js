78fd490b27159df58cddfdfb01fedc38896c2c5b
Zakia El Baaj <zakiaelbaaj@gmail.com>
2026-08-02 20:35:37 +0100

    chore: suppression script temporaire reset password

--- scripts/reset-demo-password.js
+++
@@ -1,8 +0,0 @@
-import { updatePassword } from "../services/usersService.js";
-
-const userId = 37; // Sasy26
-const newPassword = "Demo1234!";
-
-await updatePassword(userId, newPassword);
-console.log(`✅ Mot de passe réinitialisé pour user ${userId}`);
-process.exit(0);
